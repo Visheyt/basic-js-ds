b9f8b538724eaf192f654ba2ae64b0c29c512927
Alex <fornikaras7@gmail.com>
2023-12-19 16:52:23 +0500

    feat: add method add to binary-tree

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -6,16 +6,36 @@ const { NotImplementedError } = require('../extensions/index.js');
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
+class ThreeNode {
+  constructor(value){
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
 class BinarySearchTree {
-
+  constructor(){
+    this.root = null;
+  }
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return root.value
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    this.root = add(this.root,data)
+    function add(threeNode,data){
+      if (!threeNode){
+        return new ThreeNode(data)
+      }else if (threeNode.value == data){
+        return threeNode
+      }
+      if (data<node.value){
+        threeNode.left = add(threeNode.left,data)
+      }else {
+        threeNode.right = add(threeNode.right,data)
+      }
+      return threeNode
+    }
   }
 
   has(/* data */) {
