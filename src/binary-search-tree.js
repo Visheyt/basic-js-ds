b8186fd9506452fb03f10a4cc8246c489a27c712
Alex <fornikaras7@gmail.com>
2023-12-19 20:29:29 +0500

    feat: add find and has methods

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,51 +1,59 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
-class ThreeNode {
-  constructor(value){
-    this.value = value;
-    this.left = null;
-    this.right = null;
-  }
-}
+
 class BinarySearchTree {
-  constructor(){
-    this.root = null;
-  }
-  root() {
-    return root.value
+  constructor() {
+    this.element = null;
   }
 
+  root() {
+    return this.element
+  }
   add(data) {
-    this.root = add(this.root,data)
-    function add(threeNode,data){
-      if (!threeNode){
-        return new ThreeNode(data)
-      }else if (threeNode.value == data){
-        return threeNode
+    this.element = addRoot(this.element,data)
+    function addRoot(treeNode,data){
+      if (!treeNode){
+        return new Node(data)
+      }else if (treeNode.data == data){
+        return treeNode
       }
-      if (data<node.value){
-        threeNode.left = add(threeNode.left,data)
+      if (data<treeNode.data){
+        treeNode.left = addRoot(treeNode.left,data)
       }else {
-        threeNode.right = add(threeNode.right,data)
+        treeNode.right = addRoot(treeNode.right,data)
       }
-      return threeNode
+      return treeNode
     }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    let isHas = search(this.element,data)
+    function search(treeNode,value){
+      if (!treeNode)return false
+      else if(treeNode.data==value) return true
+
+      if (value<treeNode.data) return search(treeNode.left,value)
+      else return search(treeNode.right,value)
+    }
+    return isHas
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(data ) {
+    let isFind = search(this.element,data)
+    function search(treeNode,value){
+      if (!treeNode) return null
+      else if(treeNode.data==value) return value
+
+      if (value<treeNode.data) return search(treeNode.left,value)
+      else return search(treeNode.right,value)
+    }
+    return isFind
   }
 
   remove(/* data */) {
@@ -54,13 +62,19 @@ class BinarySearchTree {
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let currentRoot = this.element
+    while(currentRoot && currentRoot.left) {
+      currentRoot = currentRoot.left
+    }
+    return currentRoot.data
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let currentRoot = this.element;
+    while (currentRoot && currentRoot.right){
+      currentRoot = currentRoot.right
+    }
+    return currentRoot.data
   }
 }
 
