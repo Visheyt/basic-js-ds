92781a2d43ac27f5bcdc6bea05b02c1dc66c7845
Alex <fornikaras7@gmail.com>
2023-12-19 21:37:23 +0500

    fix: find method

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -18,16 +18,13 @@ class BinarySearchTree {
   add(data) {
     this.element = addRoot(this.element,data)
     function addRoot(treeNode,data){
-      if (!treeNode){
-        return new Node(data)
-      }else if (treeNode.data == data){
-        return treeNode
-      }
-      if (data<treeNode.data){
-        treeNode.left = addRoot(treeNode.left,data)
-      }else {
-        treeNode.right = addRoot(treeNode.right,data)
-      }
+      if (!treeNode) return new Node(data)
+      else if (treeNode.data == data) return treeNode
+
+      if (data<treeNode.data)treeNode.left = addRoot(treeNode.left,data)
+      else treeNode.right = addRoot(treeNode.right,data)
+
+
       return treeNode
     }
   }
@@ -48,7 +45,7 @@ class BinarySearchTree {
     let isFind = search(this.element,data)
     function search(treeNode,value){
       if (!treeNode) return null
-      else if(treeNode.data==value) return value
+      else if(treeNode.data==value) return treeNode
 
       if (value<treeNode.data) return search(treeNode.left,value)
       else return search(treeNode.right,value)
@@ -56,11 +53,43 @@ class BinarySearchTree {
     return isFind
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+  remove(data) {
+    this.element = removeElement(this.element,data)
+    function removeElement(treeNode,data){
+      if (!treeNode) return null
 
+      if (data<treeNode){
+        treeNode.left = removeElement(treeNode.left,data)
+        return treeNode
+      }else if(data>treeNode){
+        treeNode.right =removeElement(treeNode.right,data)
+        return treeNode
+      }else{
+          //Если элемент является листом(последним в дереве)
+          if (!treeNode.right && !treeNode.left) return null
+          //Если у него нет поддерева слева значит поднимаем правое
+          else if (!treeNode.left) {
+            treeNode = treeNode.right
+            return treeNode
+          }
+          //Если у него нет поддерева справа значит поднимаем левое
+          else if (!treeNode.right){
+            treeNode = treeNode.left
+            return treeNode
+          }
+          //Есть оба поддерева
+          else {
+            let maxFromLeft = treeNode.left
+            while(maxFromLeft.right){
+              maxFromLeft = maxFromLeft.right
+            }
+            treeNode.value = maxFromLeft.value
+            treeNode.left = removeElement(treeNode.left,maxFromLeft.value)
+            return treeNode
+          }
+      }
+    }
+  }
   min() {
     let currentRoot = this.element
     while(currentRoot && currentRoot.left) {
